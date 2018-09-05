5bc2a0ec9607d3c76b243a350525a963f1925265
kevin <kevin.nin92@gmail.com>
2018-09-05 15:50:54 +0530

    chapter : composing config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
+const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Wepack demo'
-    })
-  ]
+const parts = require('./webpack.parts');
+
+const commonConfig = merge([
+  {
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Wepack demo'
+      })
+    ]
+  }
+]);
+
+const prodConfig = merge([]);
+
+const developmentConfig = merge([
+  parts.devServer({
+    host: process.env.HOST,
+    port: process.env.PORT
+  })
+]);
+
+module.exports = (mode) => {
+  if (mode === 'production') {
+    return merge(commonConfig, prodConfig, { mode });
+  }
+
+  return merge(commonConfig, developmentConfig, { mode });
 };
